25f6f50333deb32679ef2aec902a3eb812ee89f8
israelbulcaotavares <33582958+israelbulcaotavares@users.noreply.github.com>
2022-07-30 12:22:56 -0300

    Criação do Card Car

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,16 +1,71 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
+import { RFValue } from 'react-native-responsive-fontsize';
+
+
+import Logo from '../../assets/logo.svg';
+import { Car } from '../../components/Car';
 
 import {
   Container,
-  Title
+  Header,
+  TotalCars,
+  HeaderContent,
 } from './styles';
 
-export function Home(){
+
+
+
+export function Home() {
+  const carDataOne = {
+    brand: 'Audi',
+    name: 'RS 5 Coupé',
+    rent: {
+      period: 'AO DIA',
+      price: 120,
+    },
+    thumbnail: 'https://purepng.com/public/uploads/large/purepng.com-audiaudicarluxurious-carprofessional-carwhite-audired-audi-17015274093953ufxk.png'
+  }
+  const carDataTwo = {
+    brand: 'Porsche',
+    name: 'Panamera',
+    rent: {
+      period: 'AO DIA',
+      price: 340,
+    },
+    thumbnail: 'https://purepng.com/public/uploads/large/purepng.com-black-porsche-panamera-carcarvehicletransportporsche-961524660080ezwd4.png'
+  }
+  const carDataThree = {
+    brand: 'Porsche',
+    name: 'Panamera',
+    rent: {
+      period: 'AO DIA',
+      price: 340,
+    },
+    thumbnail: 'https://purepng.com/public/uploads/large/purepng.com-black-porsche-panamera-carcarvehicletransportporsche-961524660080ezwd4.png'
+  }
+
   return (
-    <Container> 
-        <Title>
-            Home
-        </Title>
+    <Container>
+      <StatusBar
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent
+      />
+      <Header>
+        <HeaderContent>
+          <Logo
+            width={RFValue(108)}
+            height={RFValue(12)}
+          />
+          <TotalCars>
+            Total  de 12 carros
+          </TotalCars>
+        </HeaderContent>
+      </Header>
+
+      <Car data={carDataOne} />
+      <Car data={carDataTwo} />
 
     </Container>
   );
